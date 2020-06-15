e317aedb9eae385cdf7e429c468e58aa77452166
Beatriz <beaclemen90@gmail.com>
2020-06-15 10:08:17 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 // Puerto
 // =========================
 process.env.PORT = process.env.PORT || 3000;
+
+// =========================
+// Entorno
+// =========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =========================
+// Base de datos
+// =========================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://curso:7AX9J3apcmxgGith@cluster0-w2ill.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
